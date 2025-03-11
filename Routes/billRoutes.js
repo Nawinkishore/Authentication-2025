7bf652c7ad1c7ee04d29be9f4ffb8eaed5ed04ff
Nawinkishore <nawinkishore2004@gmail.com>
2025-03-11 23:19:47 +0530

    Bill Controller Updated : Update Bills are not completed

--- Routes/billRoutes.js
+++ Routes/billRoutes.js
@@ -1,6 +1,10 @@
 import express from 'express';
-import {generateBill} from "../controllers/billController.js";
+import {generateBill,getBills,getBillById,updateBill,deleteBillbyId} from "../controllers/billController.js";
 const router = express.Router();
 // Bill Routes 
 router.post('/generateBill' ,generateBill);
+router.get('/getBills',getBills);
+router.get('/getBillById/:id',getBillById);
+router.put('/updateBill/:id',updateBill);
+router.delete('/deleteBill/:id',deleteBillbyId);
 export default router;
--- controllers/billController.js
+++ controllers/billController.js
@@ -18,3 +18,54 @@ export const generateBill = async (req, res) => {
         res.status(500).json({ message: "Server Error" });
     }
 };
+
+export const getBills = async (req, res) => {
+    try {
+        const bills = await Bill.find();
+        res.status(200).json(bills);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
+
+export const getBillById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const bill = await Bill.findById(id);
+        res.status(200).json(bill);
+    }catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const updateBill = async (req, res) => {
+        const { id } = req.params;
+        try {
+            const { fromAddress, toAddress, invoiceNumber, invoiceDate, details } = req.body;
+            const bill = await Bill.findById(id);
+            if (!bill) {
+                return res.status(404).json({ message: "Bill not found" });
+            }
+    
+            bill.fromAddress = fromAddress;
+            bill.toAddress = toAddress;
+            bill.invoiceNumber = invoiceNumber;
+            bill.invoiceDate = invoiceDate;
+            bill.details = details;
+    
+            const updatedBill = await bill.save();
+            res.status(200).json(updatedBill);
+        } catch (error) {
+            res.status(500).json({ message: "Server Error" });
+        }
+}
+
+export const deleteBillbyId = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Bill.findByIdAndDelete(id);
+        res.status(200).json({ message: "Bill deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+};
